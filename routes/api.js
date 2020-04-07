265a8db26e3ed1d048fcc9918c4fbf9f2c7dff0b
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-04-06 23:36:30 -0400

    Added put and post routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 let Workout = require("../models/workout");
 
 router.get("/", (req, res) => {
@@ -22,9 +23,75 @@ router.get("/workouts", (req, res) => {
 
 })
 
+router.get("/workouts/all", (req, res) => {
+
+    Workout.find({}, (err, docs) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(docs);
+        }
+    })
+
+})
+
 // post route - create new workout, then save it
 
-//  put route - find existing workout by the _id property, add new exercise then save
+router.post("/workouts", function(req, res) {
 
+    let newWorkout = new Workout();
+
+    newWorkout.save((err) => {
+
+        console.log(err);
+        res.json(newWorkout);
+
+    });
+
+
+
+})
+
+//  put route - find existing workout by the _id property, add new exercise then save
+router.put("/workouts/:id", function(req, res) {
+
+    console.log(req.params.id, req.body);
+
+    // Workout.find({"_id": req.params.id}, (err, docs) => {
+    //     let workout = docs[0];
+    //     console.log(`This workout with id: ${workout._id} has ${workout.exercises.length} exercises`);
+
+    //     if (err) {
+    //         console.log(err);
+    //         res.json({});
+    //     } else {
+            
+    //         workout.exercises.push(req.body);
+
+    //         workout.save((err2) => {
+
+    //             console.log(err2);
+    //             res.json({});
+
+    //         });
+            
+            
+    //     }
+
+    // })
+
+    Workout.updateOne(
+      { "_id": req.params.id },
+      { $push: { "exercises" : req.body} },
+      (err) => {
+        if (err) {
+            console.log(err)
+        }
+            res.json({});
+        
+      }
+    )
+      
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const apiRoutes = require('./routes/api');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+const apiRoutes = require('./routes/api');
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/MyWorkouts", {
   useNewUrlParser: true,
